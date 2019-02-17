20a17df6d541eb59cccc14fef68f5efbbc358788
huangzhhui <huangzhwork@gmail.com>
2019-02-17 14:58:39 +0800

    Moved ApplicationContext to Utils from Framework

---
+++ src/ApplicationContext.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.org
+ * @document https://wiki.hyperf.org
+ * @contact  group@hyperf.org
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Utils;
+
+use Psr\Container\ContainerInterface;
+
+class ApplicationContext
+{
+    /**
+     * @var ContainerInterface
+     */
+    private static $container;
+
+    public static function getContainer(): ContainerInterface
+    {
+        return self::$container;
+    }
+
+    public static function setContainer(ContainerInterface $container): ContainerInterface
+    {
+        self::$container = $container;
+        return $container;
+    }
+}
